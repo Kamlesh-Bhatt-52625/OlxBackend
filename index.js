01faeb1524935e9fb3420db82f38ad34f4ab3c10
Kamlesh Bhatt <kb55262555@gmail.com>
2023-09-11 23:42:11 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const { connection } = require("./config/db");
+const { postRouter } = require("./routes/postRouter");
+const { getRouter } = require("./routes/getRouter");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to OLX!");
+});
+
+const PORT = process.env.PORT;
+// console.log(PORT);
+
+app.use("/post", postRouter);
+app.use("/get", getRouter);
+
+app.listen(PORT, async () => {
+  try {
+    await connection;
+    console.log("Connected to the DATABASE");
+  } catch (error) {
+    console.log("Error connecting to the DATABASE!");
+    console.log(error);
+  }
+  console.log(`Server is live on port ${PORT}`);
+});
